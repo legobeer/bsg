0cf56d64241f4976ee7d2f775299e879c42bea91
henwell <92310123+henwell@users.noreply.github.com>
2023-12-04 14:18:27 +0100

    Update answer.r
    10, 11 update

--- stats4/answer.r
+++ stats4/answer.r
@@ -29,12 +29,15 @@ population2 <- sum(mds_result[, 1] < 0)
 cat("Population 1 : ", population1, "%\n")
 cat("Population 2 : ", population2, "%\n")
 
+# Do you think the data
+#come from one homogeneous human population? If not, how many subpopulations do you
+#think the data might come from, and how many individuals pertain to each suppopulation?
 
 # Question 5
 cat("Stress value:", isoMDS(manhattan_dist_matrix, k = 2)$stress
 , "\n")
 # Quite big normal we have two subpopulations.
-
+# Which criterium
 # Question 6
 observed_distances <- as.vector(manhattan_dist_matrix)
 estimated_distances <- as.vector(dist(mds_result))
@@ -55,16 +58,17 @@ n <- nrow(manhattan_matrix)
 m <- ncol(manhattan_matrix)
 
 set.seed(12345)
+init <- scale(matrix(runif(m*n),ncol=m),scale=FALSE)
 random_matrix <- matrix(runif(n * m), nrow = n, ncol = m)
 nonmetric_mds_result <- isoMDS(random_matrix, k = 2)
 plot(nonmetric_mds_result$points[, 1], nonmetric_mds_result$points[, 2],
      main = "Non-Metric MDS Plot", xlab = "Dimension 1", ylab = "Dimension 2")
 text(nonmetric_mds_result$points[, 1], nonmetric_mds_result$points[, 2], labels = rownames(nonmetric_mds_result$points))
 nonmetric_mds_result$stress
-
+# Do the results support that the data come from one homogeneous population?
 # Question 8
 
-num_runs <- 150
+num_runs <- 10
 stress_result <- c()
 for (i in 1:num_runs) {
   set.seed(i)  
@@ -74,8 +78,8 @@ for (i in 1:num_runs) {
   stress_result <- c(stress_result, mds_result$stress)
 }
 
-
 plot(stress_result)
+# What do you observe
 
 # Question 9
 
@@ -90,24 +94,30 @@ for (i in 1:50) {
 }
 
 plot(stress_result)
+index_first_lt_10 <- which(stress_result < 10)
+print(index_first_lt_10[1])
 # 10
+
 num_runs <- 100
 stress_result <- c()
-point_result <- c()
+point_result <- vector("list", num_runs)
 for (i in 1:num_runs) {
   set.seed(i)  
   random_matrix <- matrix(runif(n * m), nrow = n, ncol = m)
   mds_result <- isoMDS(random_matrix, k = 2, trace=FALSE)
   
   stress_result <- c(stress_result, mds_result$stress)
-  point_result <- c(point_result, mds_result$points)
+  point_result[[i]] <- mds_result$points
 }
-cat("best:", min(stress_result$stress))
-
-best_result = point_result[which.min(stress_result)]
+cat("best:", min(stress_result))
+best_result = point_result[which.min(stress_result)][[1]]
 cat("worst:", max(stress_result))
-plot(stress_result)
+worst_result = point_result[which.max(stress_result)][[1]]
+plot(best_result[, 1], best_result[, 2], col = colors, pch = 16, main = "Best MDS Plot", xlab = "Dimension 1", ylab = "Dimension 2")
+plot(worst_result[, 1], worst_result[, 2], col = colors, pch = 16, main = "Worst MDS Plot", xlab = "Dimension 1", ylab = "Dimension 2")
+# compare results
 # 11
 
 correlation_matrix <- cor(best_result, nonmetric_mds_result$points)
 correlation_matrix
+# Comment on your findings
